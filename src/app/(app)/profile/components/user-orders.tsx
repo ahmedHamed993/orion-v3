0d69b31791ef83503ed080dc100a6f3b51300045
Ahmed Hamed <ahmedhamed@gmail.com>
2025-02-10 19:29:18 +0200

    checkout success page

--- src/app/(app)/profile/components/user-orders.tsx
+++ src/app/(app)/profile/components/user-orders.tsx
@@ -1,21 +1,21 @@
 'use client'
 import { getUserOrders } from '@/api-calls/user/getUserOrders';
+import NoItems from '@/components/no-items/no-items';
 import React, { useState, useEffect } from 'react'
 
 const UserOrders = () => {
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(true);
     const getOrders = async ()=>{
-        const data = await getUserOrders();
-        console.log('data',data);
-        setOrders(data);
+      const data = await getUserOrders();
+      setOrders(data);
     }
     useEffect(()=>{
         getOrders();
     },[])
   return (
-    <div>
-      orders
+    <div className='flex flex-col'>
+      {orders?.length === 0 ? <NoItems /> : orders.map(i => <p>{(i as any).id}</p>)}
     </div>
   )
 }
